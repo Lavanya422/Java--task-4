299167908901027c5c5ee96fc5537238f8d903bf
Lavanya422 <rlavanya.lava7@gmail.com>
2025-05-23 18:25:37 +0530

    Add files via upload

---
+++ integer1.java
@@ -0,0 +1,16 @@
+
+public class integer1 {
+
+	public static void main(String[] args) {
+        // Declare four integer variables
+        int a = 10;
+        int b = 20;
+        int c = 15;
+        int d = 5;
+
+        // Check if the sum of a and b is greater than the sum of c and d
+        if ((a + b) > (c + d)) {
+            System.out.println("The sum of a and b is greater than the sum of c and d.");
+        }
+    }
+}
